e9600ffb9a2f78cdb210ca8407337d8ac9f2e28e
Liroy BENAIM <lbenaim@e3r7p22.42.fr>
2019-09-09 16:18:34 +0200

    save

--- c03/c03_main/ex04/ft_strstr.c
+++ c03/c03_main/ex04/ft_strstr.c
@@ -1,39 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lbenaim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/09/08 19:12:14 by lbenaim           #+#    #+#             */
-/*   Updated: 2019/09/09 15:23:18 by lbenaim          ###   ########.fr       */
+/*   Created: 2019/09/09 15:24:04 by lbenaim           #+#    #+#             */
+/*   Updated: 2019/09/09 16:17:15 by lbenaim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-char *ft_strncat(char *dest, char *src, unsigned int nb)
+char	*ft_strstr(char *str, char *to_find)
 {
 	int i;
 	int n;
-
 	i = 0;
-	n = 0;
-	while (dest[i] != '\0')
+	while (str[i] != '\0')
 	{
 		i++;
 	}
-	while (src[n] != '\0' && n < nb)
-	{
-		dest[i + n] = src[n];
-		n++;
-	}
-	return(dest);
 }
 
 int main()
 {
-	char dest[50] = "frite";
-	char src[] = "steak";
-	printf("%s", ft_strncat(dest, src, 2));
+	char str[] = "";
+	char to_find[] = "":
+	printf("%s", ft_strstr(str, to_find));
 }
